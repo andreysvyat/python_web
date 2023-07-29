d49a6df4db122ac7597b7794773c873ddc148766
svyat <andreysvyatoy@gmail.com>
2023-07-29 22:40:36 +0300

    raise 404

--- task_tracker/app/views.py
+++ task_tracker/app/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest, Http404
+from django.shortcuts import render, get_object_or_404
 
 from .models import *
 
@@ -8,21 +8,24 @@ def hello_world(request: HttpRequest):
     return HttpResponse(content='Hello world')
 
 
+def show_headers(request: HttpRequest):
+    headers = [f'<br>&emsp;{key}: {value}' for key, value in request.headers.items()]
+    return HttpResponse(f'Hello from my first view<br>Headers in from your request: {"".join(headers)}')
+
+
 def hello_name(request: HttpRequest, name: str):
     return HttpResponse(content=f'Hello {name}')
 
 
 def task(reqeust, task_id):
-    t = Task.objects.get(id=task_id)
+    try:
+        t = Task.objects.get(id=task_id)
+    except Task.DoesNotExist:
+        raise Http404('Task member not found')
     return HttpResponse(render(reqeust, 'task.html', {'task': t}))
 
 
 def task_member_tasks(req, tm_id):
-    tm = TaskMember.objects.get(pk=tm_id)
+    tm = get_object_or_404(TaskMember, pk=tm_id)
     assigned_tasks = Task.objects.filter(assigned_to=tm.id)
     return HttpResponse(render(req, 'task_member.html', {'task_member': tm, 'tasks': assigned_tasks}))
-
-
-def show_headers(request: HttpRequest):
-    headers = [f'<br>&emsp;{key}: {value}' for key, value in request.headers.items()]
-    return HttpResponse(f'Hello from my first view<br>Headers in from your request: {"".join(headers)}')

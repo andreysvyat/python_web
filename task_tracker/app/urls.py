2f83704d6194341eb5b52a008a405f964d9b0571
svyat <andreysvyatoy@gmail.com>
2023-07-30 17:41:53 +0300

    named urls

--- task_tracker/app/urls.py
+++ task_tracker/app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('hello', views.hello_world),
     path("headers", views.show_headers),
     path('hello/<str:name>', views.hello_name),
-    path('tasks/<int:task_id>', views.task),
+    path('tasks/<int:task_id>', views.task, name='task'),
     path('task_members/<int:tm_id>/tasks', views.task_member_tasks)
 ]
